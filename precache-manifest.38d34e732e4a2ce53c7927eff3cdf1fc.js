73eeebaea6d62dc24c66b4fa05b5c228def86eaf
fed-gren <deveb22@gmail.com>
2019-03-25 14:01:17 +0900

    Updates

---
+++ precache-manifest.38d34e732e4a2ce53c7927eff3cdf1fc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "7952d9d4b252ca1c580e",
+    "url": "/movie_app/static/js/runtime~main.e6fc6960.js"
+  },
+  {
+    "revision": "68b9f8d20cb2ecc2a68f",
+    "url": "/movie_app/static/js/main.51a3d01f.chunk.js"
+  },
+  {
+    "revision": "b50b70481e48ce448ee1",
+    "url": "/movie_app/static/js/2.5b80cee0.chunk.js"
+  },
+  {
+    "revision": "68b9f8d20cb2ecc2a68f",
+    "url": "/movie_app/static/css/main.8f689d20.chunk.css"
+  },
+  {
+    "revision": "c3fe4adf48774c7db00eecdb7ddb6a5a",
+    "url": "/movie_app/index.html"
+  }
+];
